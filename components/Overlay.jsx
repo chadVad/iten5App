9137d667078294af66233eeb2ec105d6a6097ca7
chadVad <134967319+chadVad@users.noreply.github.com>
2023-05-30 19:18:39 -0300

    Tested Site App
    Second version of the site

---
+++ components/Overlay.jsx
@@ -0,0 +1,20 @@
+import { Scroll } from "@react-three/drei";
+
+
+
+export const Overlay = () => {
+    return (
+        <Scroll html>
+            <h3 className="font-sansserif text-2xl">
+                Testing Fonts no avail
+            </h3>
+            <p>
+            </p>
+            <p className="font-serif text-2xl">Testing CSS</p>
+            <h3 className="font-cursive text-2xl">
+                Testing Fonts no avail
+            </h3>
+            <a href = "https://www.duckduckgo.com">Testing Links</a>
+        </Scroll>
+    )
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            serif: ["Playfair Display", ...defaultTheme.fontFamily.sans],
+            cursive: ["Lobster", ...defaultTheme.fontFamily.sans],
+            sansserif: ["Varela Round", ...defaultTheme.fontFamily.sans],
+        },
+    },
+    plugins: [],
+};
